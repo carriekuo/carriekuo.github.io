ff2e342d62b94a4c3e0c3c94dc81b590902deff6
carriekuo <kuoc965@newschool.edu>
2023-11-14 12:57:47 -0500

    update

---
+++ entries/entry8/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $('button').click(function() {
+        $('.measurements').toggle();
+    });
+});
+
+$(document).ready(function() {
+    $('button').click(function() {
+        $('.soupbutton').toggle();
+    });
+});
+
+$(document).ready(function() {
+    $('button').click(function() {
+        $('.dumplingbutton').toggle();
+    });
+});
+
+$(document).ready(function() {
+    $('button').click(function() {
+        $('.servebutton').toggle();
+    });
+});
